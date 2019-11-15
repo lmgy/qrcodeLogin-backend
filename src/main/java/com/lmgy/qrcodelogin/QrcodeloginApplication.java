48499bbe1e3dd9cc78a3d49f160f8b9b1884633f
lmgy <luomusue@gmail.com>
2019-11-15 11:24:14 +0800

    Initial commit

---
+++ src/main/java/com/lmgy/qrcodelogin/QrcodeloginApplication.java
@@ -0,0 +1,13 @@
+package com.lmgy.qrcodelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeloginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lmgy/qrcodelogin/QrcodeloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmgy.qrcodelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
